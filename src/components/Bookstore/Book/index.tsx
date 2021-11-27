fccfe99d4c92b70331cb49d4ad309aa09a3fe446
HenryN <hyrre9056@gmail.com>
2021-11-27 02:06:54 -0500

    [ADD] FAV button, working with localStorage

--- src/components/Bookstore/Book/index.tsx
+++ src/components/Bookstore/Book/index.tsx
@@ -3,6 +3,7 @@ import { Modal, Button as Btn, Form } from 'react-bootstrap';
 import useLocalStorage from '../../../hooks/useLocalStorage'
 import './book.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiFillHeart } from 'react-icons/ai'
 
 
 export type BsBookType = {
@@ -11,6 +12,7 @@ export type BsBookType = {
   author: string
   content_short: string
   thumbnail: string
+  isFav: boolean
 }
 
 export interface BsBookProps {
@@ -42,7 +44,7 @@ function MyVerticallyCenteredModal(props: any) {
           Descripción: {props.book.content_short}
         </p>
         <Form className="formFlex">
-          <Form.Label htmlFor="com">Añadir comentario:</Form.Label>
+          <Form.Label htmlFor="com">Agregar comentario:</Form.Label>
           <Form.Control type="text" />
           <Btn>+</Btn>
         </Form>
@@ -51,6 +53,7 @@ function MyVerticallyCenteredModal(props: any) {
   );
 }
 
+// onClick={props.addFav(props.book.ID, props.book.title)}
 
 const BsBook = ({ book }: BsBookProps) => {
   const [modalShow, setModalShow] = React.useState(false);
@@ -67,7 +70,7 @@ const BsBook = ({ book }: BsBookProps) => {
   }
 
   const removeFav = (ID: String) => {
-    const newFavs = favs.filter((t:any) => t.id !== ID)
+    const newFavs = favs.filter((t: any) => t.id !== ID)
     setFavs(newFavs)
   }
 
@@ -79,23 +82,31 @@ const BsBook = ({ book }: BsBookProps) => {
     setComments([...comments, newComment]);
   }
 
+  const handleClic = () => {
+    addFav(book.ID, book.title)
+  }
+
   return (
     <>
-      <div className="book" onClick={() => setModalShow(true)}>
-        <div className="book-image">
+      {book.isFav = favs.filter(function (t:any) { return (t.id === book.ID) }).length>0}
+      <div className="book" >
+        <div className="book-image" onClick={() => setModalShow(true)}>
           {book.thumbnail ? <img
             alt={book.title}
             src={book.thumbnail} />
             : <img src="https://picsum.photos/200/260" alt="default" />}
         </div>
         <p className="book-title">{book.title}</p>
-        <button onClick={() => addFav(book.ID, book.title)}>Agregar a favs</button>
+        <span className="fav-span" onClick={handleClic}>
+          <AiFillHeart className={`fav-icon${book.isFav ? '--red' : ''}`} /> 
+        </span>
       </div>
 
       <MyVerticallyCenteredModal
         show={modalShow}
         onHide={() => setModalShow(false)}
         book={book}
+        removeFav={removeFav}
       />
     </>
   )
