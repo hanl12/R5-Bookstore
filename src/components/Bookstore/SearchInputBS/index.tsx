d0f7ee4f81fef7fb4dc8d49e78da2a607f033847
HenryN <hyrre9056@gmail.com>
2021-11-28 22:11:52 -0500

    [ADD] State in bookstore

--- src/components/Bookstore/SearchInputBS/index.tsx
+++ src/components/Bookstore/SearchInputBS/index.tsx
@@ -1,33 +1,22 @@
-import React, { ChangeEvent, useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { ChangeEvent, useState } from 'react'
 import './SearchInput.css'
 
 interface SearchInputProps {
-    setResponse: Function
+    setSearch: Function
 }
 
-const SearchInputBS = ({ setResponse }: SearchInputProps) => {
+const SearchInputBS = ({ setSearch }: SearchInputProps) => {
     const [searchValue, setSearchValue] = useState('')
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
         setSearchValue(event.target.value)
     }
 
-    function getBooks(title: string = 'javascript') {
-        setResponse({})
-        axios.get(`https://www.etnassoft.com/api/v1/get/?keyword=${title}`).then((response) => setResponse(response))
-    }
-
-    useEffect(() => {
-        getBooks()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
     return (
         <div className="search">
             <h1>BOOKSTORE</h1>
             <input type="text" className="search-input" placeholder="Buscar un libro" value={searchValue} onChange={handleInputChange} />
-            <button className="search-button" onClick={() => getBooks(searchValue)}>Buscar</button>
+            <button className="search-button" onClick={() => setSearch(searchValue)}>Buscar</button>
         </div>
     )
 }
--- src/containers/Bookstore/index.tsx
+++ src/containers/Bookstore/index.tsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Spinner } from 'react-bootstrap'
-import { BsBookType } from '../../components/Bookstore/Book'
+import axios from 'axios';
 import BsBooks from '../../components/Bookstore/Books'
 import SearchInputBS from '../../components/Bookstore/SearchInputBS'
 
 interface Response {
-    data?: BsBookType[]
+    data?: [{
+        ID: string
+        title: string
+        author: string
+        content_short: string
+        thumbnail: string
+        isFav: boolean
+        comment: String
+    }]
 }
 
 const Bookstore = () => {
     const [response, setResponse] = useState<Response>()
+    const [search, setSearch] = useState('javascript');
+
+    function getBooks(title: string) {
+        setResponse({});
+        axios.get(`https://www.etnassoft.com/api/v1/get/?keyword=${title}`).then((response) => setResponse(response))
+    }
+
+    useEffect(() => {
+        getBooks(search);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search])
+
     return (
         <div>
-            <SearchInputBS setResponse={setResponse} />
-            {response?.data ? <BsBooks books={response.data} /> : <Spinner style={{position: 'absolute', top: '50%', left: '50%'}} animation="border" role="status">
+            <SearchInputBS setSearch={setSearch} />
+            {response?.data ? <BsBooks books={response.data} /> : <Spinner style={{ position: 'absolute', top: '50%', left: '50%' }} animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
             </Spinner>}
         </div>
